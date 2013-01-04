bf96671bd44d2dcf17c736c25da0b64ac9060ffe
soranchu <soranchu@gmail.com>
2013-01-04 18:32:53 +0900

    init

---
+++ ComicWatcher/src/jp/tande/android/comicwatcher/TopListActivity.java
@@ -0,0 +1,22 @@
+package jp.tande.android.comicwatcher;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class TopListActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.top_list);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.top_list, menu);
+        return true;
+    }
+    
+}
